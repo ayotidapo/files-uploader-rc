60e6d2b27307e8501573100dccd06721d1141df3
otidapson <oladapo.o@softcom.ng>
2021-01-02 12:27:03 +0100

    update packege json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  console.log(12)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is a react library for Ui file upload showing progress bar, <br />it will be very flexible for almost all situations.
+        Trust Me!
       </header>
     </div>
   );
