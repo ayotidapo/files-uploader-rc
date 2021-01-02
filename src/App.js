545529d89feefc523cfe13edae20804e88b667d6
otidapson <oladapo.o@softcom.ng>
2021-01-02 14:45:49 +0100

    o

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css'
 import React from 'react';
+import './App.css';
 function App() {
-  console.log(12)
+  console.log(12);
   return (
     <div className="App">
       <header className="App-header">
-        This is a react library for Ui file upload showing progress bar, <br />it will be very flexible for almost all situations.
-        Trust Me!
+        This is a react library for Ui file upload showing progress bar, <br />
+        it will be very flexible for almost all situations. Trust Me!
       </header>
     </div>
   );
